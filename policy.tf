88bca24aa8f32432e5258f47f5849f6cd175bd33
HamiltonHord <55853225+HamiltonHord@users.noreply.github.com>
2026-01-23 04:12:00 -0800

    Adding Terraform Version to Tags sent to Datadog (#16)

--- policy.tf
+++ policy.tf
@@ -1,16 +1,17 @@
 locals {
   common_tags = {
-    "account":         "[input.account.name]",
-    "branch":          "[input.run_updated.run.commit.branch]",
-    "drift_detection": "[input.run_updated.run.drift_detection]",
-    "run_note":        "[input.run_updated.note]",
-    "run_type":        "[lower(input.run_updated.run.type)]",
-    "run_url":         "[input.run_updated.urls.run]",
-    "final_state":     "[lower(run_state)]",
-    "space":           "[lower(input.run_updated.stack.space.id)]",
-    "stack":           "[lower(input.run_updated.stack.id)]",
-    "triggered_by":    "[input.run_updated.run.triggered_by]",
-    "worker_pool":     "[worker_pool]",
+    "account":           "[input.account.name]",
+    "branch":            "[input.run_updated.run.commit.branch]",
+    "drift_detection":   "[input.run_updated.run.drift_detection]",
+    "run_note":          "[input.run_updated.note]",
+    "run_type":          "[lower(input.run_updated.run.type)]",
+    "run_url":           "[input.run_updated.urls.run]",
+    "final_state":       "[lower(run_state)]",
+    "space":             "[lower(input.run_updated.stack.space.id)]",
+    "stack":             "[lower(input.run_updated.stack.id)]",
+    "terraform_version": "[input.run_updated.stack.terraform_version]".
+    "triggered_by":      "[input.run_updated.run.triggered_by]",
+    "worker_pool":       "[worker_pool]",
   }
 }
 
