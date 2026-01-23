cdb222732af0e6f07ea499b64fe5547ca757c483
tomasmik <tomasmik@protonmail.com>
2026-01-23 14:18:36 +0200

    Fix the previous PR using a `.` instead of `,`

--- policy.tf
+++ policy.tf
@@ -9,7 +9,7 @@ locals {
     "final_state":       "[lower(run_state)]",
     "space":             "[lower(input.run_updated.stack.space.id)]",
     "stack":             "[lower(input.run_updated.stack.id)]",
-    "terraform_version": "[input.run_updated.stack.terraform_version]".
+    "terraform_version": "[input.run_updated.stack.terraform_version]",
     "triggered_by":      "[input.run_updated.run.triggered_by]",
     "worker_pool":       "[worker_pool]",
   }
